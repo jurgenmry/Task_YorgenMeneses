a484c66d19c34c6abff0fbcacd01a755d5bf8874
Jurgen Meneses <jurgenmeneses@gmail.com>
2025-03-15 14:28:23 +0200

    feat(input): add input config
     -Implemented an InputConfig asset to define and manage input binding
    - Ensured compatibility with the AbilitySystemComponent for ability activation.
    - Allows for easier customization and rebinding of player controls.

--- Source/YorgenMeneses_Task/Input/Task_InputConfig.cpp
+++ Source/YorgenMeneses_Task/Input/Task_InputConfig.cpp
@@ -1,5 +1,30 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "Input/Task_InputConfig.h"
 
+const UInputAction* UTask_InputConfig::FindNativeInputActionForTag(const FGameplayTag& InputTag) const
+{
+	for (const FESInputAction& Action : NativeInputActions)
+	{
+		if (Action.InputAction && (Action.InputTag == InputTag))
+		{
+			return Action.InputAction;
+		}
+	}
+	UE_LOG(LogTemp, Error, TEXT("Can't find NativeInputAction for InputTag [%s] on InputConfig [%s]."), *InputTag.ToString(), *GetNameSafe(this));
+
+	return nullptr;
+}
+
+const UInputAction* UTask_InputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag) const
+{
+	for (const FESInputAction& Action : AbilityInputActions)
+	{
+		if (Action.InputAction && (Action.InputTag == InputTag))
+		{
+			return Action.InputAction;
+		}
+	}
+	UE_LOG(LogTemp, Error, TEXT("Can't find NativeInputAction for InputTag [%s] on InputConfig [%s]."), *InputTag.ToString(), *GetNameSafe(this));
+
+	return nullptr;
+}
--- Source/YorgenMeneses_Task/Input/Task_InputConfig.h
+++ Source/YorgenMeneses_Task/Input/Task_InputConfig.h
@@ -1,17 +1,45 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
 #include "Task_InputConfig.generated.h"
 
-/**
- * 
- */
+class UInputAction;
+//FISInputAction
+//Struct used to map an input action to a gameplay input tag.
+ 
+USTRUCT(BlueprintType)
+struct FESInputAction
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly)
+	const UInputAction* InputAction = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, Meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+};
+
 UCLASS()
 class YORGENMENESES_TASK_API UTask_InputConfig : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
+
+	const UInputAction* FindNativeInputActionForTag(const FGameplayTag& InputTag) const;
+	const UInputAction* FindAbilityInputActionForTag(const FGameplayTag& InputTag) const;
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (TitleProperty = "InputAction"))
+	TArray<FESInputAction> NativeInputActions;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (TitleProperty = "InputAction"))
+	TArray<FESInputAction> AbilityInputActions;
 	
 };
