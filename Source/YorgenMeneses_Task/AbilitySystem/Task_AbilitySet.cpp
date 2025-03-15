1cdf036db0d45b100e63590467f232b391c6166f
Jurgen Meneses <jurgenmeneses@gmail.com>
2025-03-15 14:23:50 +0200

    feat(ability-system): add ability set DataTable for assigning abilities to player state
    - Implemented functionality to load abilities dynamically from the DataTable.
    - Ensured compatibility with the AbilitySystemComponent for automatic ability registration.
    - Provides a scalable approach for managing abilities without hardcoded assignments.

--- Source/YorgenMeneses_Task/AbilitySystem/Task_AbilitySet.cpp
+++ Source/YorgenMeneses_Task/AbilitySystem/Task_AbilitySet.cpp
@@ -2,4 +2,49 @@
 
 
 #include "AbilitySystem/Task_AbilitySet.h"
+#include "GameplayAbilitySpecHandle.h"
+#include "AbilitySystem/Task_AbilitySystemComponent.h"
+#include "AbilitySystem/Task_BaseGameplayAbility.h"
 
+void FSAbilitySet_GrantedHandles::AddAbilitySpecHandle(const FGameplayAbilitySpecHandle& Handle)
+{
+	if (Handle.IsValid())
+	{
+		AbilitySpecHandles.Add(Handle);
+	}
+}
+
+void UTask_AbilitySet::GiveToAbilitySystem(UTask_AbilitySystemComponent* InASC, FSAbilitySet_GrantedHandles* OutGrantedHandles, UObject* SourceObject) const
+{
+	check(InASC);
+
+	if (!InASC->IsOwnerActorAuthoritative())
+	{
+		// Must be authoritative to give or take ability sets.
+		return;
+	}
+
+	for (int32 AbilityIndex = 0; AbilityIndex < GrantedGameplayAbilities.Num(); ++AbilityIndex)
+	{
+		const FSAbilitySet_GameplayAbility& AbilityToGrant = GrantedGameplayAbilities[AbilityIndex];
+
+		if (!IsValid(AbilityToGrant.Ability))
+		{
+			UE_LOG(LogTemp, Error, TEXT("GrantedGameplayAbilities[%d] on ability set [%s] is not valid."), AbilityIndex, *GetNameSafe(this));
+			continue;
+		}
+
+		UTask_BaseGameplayAbility* AbilityCDO = AbilityToGrant.Ability->GetDefaultObject<UTask_BaseGameplayAbility>();
+
+		FGameplayAbilitySpec AbilitySpec(AbilityCDO, AbilityToGrant.AbilityLevel);
+		AbilitySpec.SourceObject = SourceObject;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilityToGrant.InputTag);
+
+		const FGameplayAbilitySpecHandle AbilitySpecHandle = InASC->GiveAbility(AbilitySpec);
+
+		if (OutGrantedHandles)
+		{
+			OutGrantedHandles->AddAbilitySpecHandle(AbilitySpecHandle);
+		}
+	}
+}
--- Source/YorgenMeneses_Task/AbilitySystem/Task_AbilitySet.h
+++ Source/YorgenMeneses_Task/AbilitySystem/Task_AbilitySet.h
@@ -1,17 +1,64 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
+#include "GameplayAbilitySpecHandle.h"
+#include "GameplayTagContainer.h"
 #include "Task_AbilitySet.generated.h"
 
-/**
- * 
- */
+class UTask_AbilitySystemComponent;
+class UTask_BaseGameplayAbility;
+
+USTRUCT(BlueprintType)
+struct FSAbilitySet_GameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	// Gameplay ability to grant.
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UTask_AbilitySystemComponent> Ability = nullptr;
+
+	// Level of ability to grant.
+	UPROPERTY(EditDefaultsOnly)
+	int32 AbilityLevel = 1;
+
+	// Tag used to process input for the ability.
+	UPROPERTY(EditDefaultsOnly, Meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+};
+
+USTRUCT(BlueprintType)
+struct FSAbilitySet_GrantedHandles
+{
+	GENERATED_BODY()
+
+public:
+void AddAbilitySpecHandle(const FGameplayAbilitySpecHandle& Handle);
+
+protected:
+	UPROPERTY()
+	TArray<FGameplayAbilitySpecHandle> AbilitySpecHandles;
+};
+
 UCLASS()
 class YORGENMENESES_TASK_API UTask_AbilitySet : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
+
+	// Grants the ability set to the specified ability system component.
+	// The returned handles can be used later to take away anything that was granted.
+	void GiveToAbilitySystem(UTask_AbilitySystemComponent* InASC, FSAbilitySet_GrantedHandles* OutGrantedHandles, UObject* SourceObject = nullptr) const;
+
+protected:
+
+	// Gameplay abilities to grant when this ability set is granted.
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay Abilities", meta = (TitleProperty = Ability))
+	TArray<FSAbilitySet_GameplayAbility> GrantedGameplayAbilities;
 	
 };
